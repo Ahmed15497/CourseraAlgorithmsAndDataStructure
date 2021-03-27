2b5e14c852c670838420fabcbf391bcf088a4dab
Ahmed <ahmedsaad22502145@gmail.com>
2021-03-27 15:00:56 +0200

    'solving least common multiple'

--- course 1 - Algorithmic toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ course 1 - Algorithmic toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,40 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+
+int gcd_fast(int a, int b) {
+  int temp = 0;
+	if (a < b){
+    temp = a;
+    a = b;
+    b = temp;
+  }
+  if (b == 0){
+    return a;
+  }else{
+    temp = a % b;
+    a = b;
+    b = temp;
+
+  }
+
+	return gcd_fast(a,b);
+}
+
+
+long long lcm_fast(int a, int b) {
+
+  int gcd = gcd_fast(a, b);
+  int b_new = b / gcd;
+  long long lcm = (long long) a * b_new;
+  return lcm;
+}
+
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
+
